c37fef2a57ea8336c90892ebb314f9eb93236cef
Caleb Jephuneh <calebjephuneh@gmail.com>
2025-03-28 14:55:26 +0300

    test

--- backend/utils/response.js
+++
@@ -1,8 +0,0 @@
-exports.successResponse = (res, message, data) => {
-    res.json({ success: true, message, data });
-  };
-  
-  exports.errorResponse = (res, message, code = 400) => {
-    res.status(code).json({ success: false, message });
-  };
-  
