b236ba96bd16865c400efc8f169c82f1e1a195f1
Caleb Jephuneh <calebjephuneh@gmail.com>
2025-03-28 14:25:15 +0300

    feat: add ws dependency and implement WebSocket server structure

--- backend/services/notification.service.js
+++ backend/services/notification.service.js
@@ -1,31 +1,31 @@
-const WebSocket = require("ws");
+// const WebSocket = require("ws");
 
-const clients = new Set();
+// const clients = new Set();
 
-const initWebSocket = (server) => {
-  const wss = new WebSocket.Server({ server });
+// const initWebSocket = (server) => {
+//   const wss = new WebSocket.Server({ server });
 
-  wss.on("connection", (ws) => {
-    clients.add(ws);
+//   wss.on("connection", (ws) => {
+//     clients.add(ws);
 
-    ws.on("message", (message) => {
-      console.log("Received:", message);
-    });
+//     ws.on("message", (message) => {
+//       console.log("Received:", message);
+//     });
 
-    ws.on("close", () => {
-      clients.delete(ws);
-    });
-  });
+//     ws.on("close", () => {
+//       clients.delete(ws);
+//     });
+//   });
 
-  console.log("📡 WebSocket server initialized");
-};
+//   console.log("📡 WebSocket server initialized");
+// };
 
-const sendNotification = (message) => {
-  clients.forEach((client) => {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(JSON.stringify({ message }));
-    }
-  });
-};
+// const sendNotification = (message) => {
+//   clients.forEach((client) => {
+//     if (client.readyState === WebSocket.OPEN) {
+//       client.send(JSON.stringify({ message }));
+//     }
+//   });
+// };
 
-module.exports = { initWebSocket, sendNotification };
+// module.exports = { initWebSocket, sendNotification };
